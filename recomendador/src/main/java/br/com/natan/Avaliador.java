5d046eeacbe978121e4c3b77672ab8e2da131dc3
Natan Fagundes <natan.fags@gmail.com>
2018-11-28 10:23:31 -0200

    {recomendador}ADD avaliador para teste de erros

---
+++ recomendador/src/main/java/br/com/natan/Avaliador.java
@@ -0,0 +1,26 @@
+package br.com.natan;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
+import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.model.DataModel;
+
+public class Avaliador {
+
+	public static void main(String[] args) throws TasteException, IOException {
+
+		File file = new File("dados.csv");
+		DataModel model = new FileDataModel(file);
+
+		RecommenderEvaluator evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator();
+		RecommenderBuilder builder = new RecomendadorDeProdutosBuilder();
+		double erro = evaluator.evaluate(builder, null, model, 0.9, 1.0);
+		System.out.println(erro);
+
+	}
+}
